88a56e7e1e217e4c84add5523dd43e199ab33a45
Radoslav Simeonov <rsimeonovv@abv.bg>
2020-11-18 19:46:56 +0200

    Optimized, printing common projects and modified for the scenario where one employee works on a project more than once.

--- Sirma-Task2/Sirma-Task2/Program.cs
+++ Sirma-Task2/Sirma-Task2/Program.cs
@@ -8,8 +8,14 @@ namespace Sirma_Task
 {
     class Program
     {
-
-        //method to calculate the common period between two sets of dates
+        /// <summary>
+        /// Method to calculate the common period of time between two sets of dates.
+        /// </summary>
+        /// <param name="s1">First set date from</param>
+        /// <param name="e1">First set date to</param>
+        /// <param name="s2">Second set date from</param>
+        /// <param name="e2">Second set date to</param>
+        /// <returns>Returns the calculated days.</returns>
         private static int InclusiveDays(DateTime s1, DateTime e1, DateTime s2, DateTime e2)
         {
             // If they don't intersect return 0.
@@ -25,18 +31,62 @@ namespace Sirma_Task
             return (int)(end - start).TotalDays + 1;
         }
 
+       /// <summary>
+       /// Method to convert the date if needed to today's date if its NULL.
+       /// </summary>
+       /// <param name="date">Date to be converted</param>
+       /// <returns>Returns the converted or non-converted date.</returns>
+        private static string ConvertDate(string date)
+        {
+            if(date == "NULL")
+            {
+                return date = DateTime.Today.ToString();
+            }
+
+            return date;
+        }
+
+        /// <summary>
+        /// Method to calculate the total amount of days worked between two employees on common projects.
+        /// It uses ConvertDate method to get the correct date and InclusiveDays method to find the common days between two sets of dates.
+        /// </summary>
+        /// <param name="emps1">List of first employee common projects</param>
+        /// <param name="emps2">List of second employee common projects</param>
+        /// <returns> Return days worked on common projects between two employees. </returns>
+        private static int CommonEmployeesDays(List <ProjectWorked> emps1, List <ProjectWorked> emps2)
+        {
+            var totalDays = 0;
+            foreach (var emp1 in emps1)
+            {
+                foreach (var emp2 in emps2)
+                {
+                    emp1.DateFrom = ConvertDate(emp1.DateFrom);
+                    emp1.DateTo = ConvertDate(emp1.DateTo);
+                    emp2.DateFrom = ConvertDate(emp2.DateFrom);
+                    emp2.DateTo = ConvertDate(emp2.DateTo);
+
+                    var calcInterDays = InclusiveDays(Convert.ToDateTime(emp1.DateFrom), Convert.ToDateTime(emp1.DateTo),
+                    Convert.ToDateTime(emp2.DateFrom), Convert.ToDateTime(emp2.DateTo));
+
+                    totalDays += calcInterDays;
+                }
+            }
+            return totalDays;
+        }
+
+
         static void Main(string[] args)
         {
             var counter = 0;
             string line;
-      
-            Dictionary<int, List<ProjectWorked>> employees = new Dictionary<int, List<ProjectWorked>>();
 
-            string workingDirectory = Environment.CurrentDirectory;
-            string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+            var employees = new Dictionary<int, List<ProjectWorked>>();
+
+            var workingDirectory = Environment.CurrentDirectory;
+            var projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
 
             //file in main directory with the records
-            StreamReader file =
+            var file =
                 new StreamReader(projectDirectory + "\\employees.txt");
 
             //filling the dictionary with key - Employee ID and value - list of ProjectWorked - Project ID, DateFrom and DateTo
@@ -60,67 +110,47 @@ namespace Sirma_Task
             file.Close();
 
             var maxDays = 0;
+            var projectsWorkedOn = new List<int>();
+            var totalDaysWorked = 0;
             var topEmp1Id = 0;
             var topEmp2Id = 0;
+            var daysWorked = 0;
 
-            /*
+            /*         
              * First we find the common project ID's between different employees added by ID in the dictionary for key.
-             * Then we get the two employees from the list with their projects and dates who are already added in the dictionary.
-             * We check if some of their dates are NULL and if they are, we make them the day today.
-             * Then we use the private method who finds the number of common days between two sets of dates and add them into the total days.
-             * We repeat the same for every two different employees and always store the current biggest number of days and the two employee ID's
-             * And we print them to the console at the end.
+             * Then we get the list of employees from the list with their projects and dates who are already added in the dictionary.
              */
             foreach (var id in employees.Keys)
             {
                 foreach (var id2 in employees.Keys)
                 {
-                    var totalDays = 0;
                     if (id != id2)
                     {
-                        var commons = employees[id].Select(e => e.ProjectId).ToList()
+                        var commonListOfProjects = employees[id].Select(e => e.ProjectId).ToList()
                             .Intersect(employees[id2].Select(e2 => e2.ProjectId).ToList()).ToList();
 
-                        foreach (var commonProjectId in commons)
+                        foreach (var commonProjectId in commonListOfProjects)
                         {
-                            var firstEmp = employees[id].FirstOrDefault(e => e.ProjectId == commonProjectId);
-                            var secondEmp = employees[id2].FirstOrDefault(e => e.ProjectId == commonProjectId);
-                            List<ProjectWorked> bothEmps = new List<ProjectWorked> { firstEmp, secondEmp };
+                            var firstEmps = employees[id].Where(e => e.ProjectId == commonProjectId).ToList();
+                            var secondEmps = employees[id2].Where(e => e.ProjectId == commonProjectId).ToList();
 
-                            if (firstEmp.DateTo == "NULL")
-                            {
-                                firstEmp.DateTo = DateTime.Today.ToString();
-                            }
-                            if (firstEmp.DateFrom == "NULL")
-                            {
-                                firstEmp.DateFrom = DateTime.Today.ToString();
-                            }
-                            if (secondEmp.DateTo == "NULL")
-                            {
-                                secondEmp.DateTo = DateTime.Today.ToString();
-                            }
-                            if (secondEmp.DateFrom == "NULL")
-                            {
-                                secondEmp.DateFrom = DateTime.Today.ToString();
-                            }
-
-                            var calcInterDays = InclusiveDays(Convert.ToDateTime(firstEmp.DateFrom), Convert.ToDateTime(firstEmp.DateTo),
-                                Convert.ToDateTime(secondEmp.DateFrom), Convert.ToDateTime(secondEmp.DateTo));
-
-                            totalDays += calcInterDays;
-             
+                            daysWorked = CommonEmployeesDays(firstEmps, secondEmps);
+                            totalDaysWorked += daysWorked;
+                        
                         }
-                        if (totalDays > maxDays)
+                        if (totalDaysWorked > maxDays)
                         {
-                            maxDays = totalDays;
+                            projectsWorkedOn = commonListOfProjects;
+                            maxDays = totalDaysWorked;
                             topEmp1Id = id;
                             topEmp2Id = id2;
                         }
                     }
-                    totalDays = 0;
+                    totalDaysWorked = 0;
                 }
             }
-            Console.WriteLine("Longest days worked on common projects: " + maxDays + " First employee ID=" + topEmp1Id + " Second employee ID=" + topEmp2Id);
+            Console.WriteLine("Employee " + topEmp1Id + " and employee "  + topEmp2Id + " worked on projects: " + (string.Join("," , projectsWorkedOn))
+                + " for a total of " + maxDays + " days maximum.");
 
         }
     }
